c526ff63b1f163999e3caf67442c735d3bcee6be
HalefCaetano <halef_caetano@hotmail.com>
2024-01-03 16:50:36 -0800

    Final App

---
+++ src/studentdatabaseapp/StudentDatabaseapp.java
@@ -0,0 +1,26 @@
+package studentdatabaseapp;
+
+import java.util.Scanner;
+
+public class StudentDatabaseapp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Enter number of new students to enroll: ");
+		Scanner in = new Scanner(System.in);
+		int numOfStudents = in.nextInt();
+		Student[] students = new Student[numOfStudents];
+		
+		for(int n = 0; n < numOfStudents; n++) {
+			students[n] = new Student();
+			students[n].enroll();
+			students[n].payTuition();
+		}
+		
+		for(int n = 0; n < numOfStudents; n++) {
+			System.out.println(students[n].toString());
+		}
+
+	}
+
+}
